a8982214309e6dcd1982ebb85463b748d81128b7
leticea <nascimento.leticeah@gmail.com>
2023-01-26 19:10:54 -0300

    editando icones

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,22 @@
-import { View } from "react-native";
+import { View, TouchableOpacity, Text } from "react-native";
+import { Feather } from "@expo/vector-icons";
+import colors from "tailwindcss/colors";
+
+import Logo from "../assets/logo.svg";
 
 export function Header() {
-  return <View></View>;
+  return (
+    <View className="w-full flex-row items-center justify-between">
+      <Logo />
+
+      <TouchableOpacity
+        activeOpacity={0.7}
+        className="flex-row h-11 px-4 border border-violet-500 rounded-lg items-center"
+      >
+        <Feather name="plus" color={colors.violet[500]} size={20} />
+
+        <Text className="text-white ml-3 font-semibold text-base">Novo</Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
