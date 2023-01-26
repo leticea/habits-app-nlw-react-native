fcabc3739d8957bdab33c246ea24dc60fe2f2f14
leticea <nascimento.leticeah@gmail.com>
2023-01-26 19:20:57 -0300

    criando os quadradinhos

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,14 @@
 import { View } from "react-native";
+
+import { HabitDay } from "../components/HabitDay";
 import { Header } from "../components/Header";
 
 export function Home() {
   return (
     <View className="flex-1 bg-background px-8 pt-16">
       <Header />
+
+      <HabitDay />
 
     </View>
   )
