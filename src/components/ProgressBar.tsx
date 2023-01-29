fe5cfa3f8319983af14c02e850b0deb5750b0011
leticea <nascimento.leticeah@gmail.com>
2023-01-29 11:14:54 -0300

    inserindo react animated para animar o progress bar e checkbox

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -1,5 +1,10 @@
+import { useEffect } from "react";
 import { View } from "react-native";
-import Animated, { useAnimatedStyle, useSharedValue } from "react-native-reanimated";
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from "react-native-reanimated";
 
 interface Props {
   progress?: number;
@@ -10,15 +15,17 @@ export function ProgressBar({ progress = 0 }: Props) {
 
   const style = useAnimatedStyle(() => {
     return {
-      width: `${sharedProgress.value}%`
+      width: `${sharedProgress.value}%`,
     };
   });
+
+  useEffect(() => {
+    sharedProgress.value = withTiming(progress);
+  }, [progress]);
+
   return (
     <View className="w-full h-3 rounded-xl bg-zinc-700 mt-4">
-      <Animated.View
-        className="h-3 rounded-xl bg-violet-600"
-        style={style}
-      />
+      <Animated.View className="h-3 rounded-xl bg-violet-600" style={style} />
     </View>
   );
 }
